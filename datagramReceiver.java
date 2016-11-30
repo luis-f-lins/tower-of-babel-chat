0171156a49c5aa5e82ae54d26a8f86b40de50990
Paulo <paulosanz@poli.ufrj.br>
2016-11-29 23:31:07 -0200

    Receiving multiple messages

--- datagramReceiver.java
+++ datagramReceiver.java
@@ -3,14 +3,16 @@ import java.io.*;
 
 class datagramReceiver{
 	public static void main(String[ ] args){
-		try{
-			int MAX_LEN = 40;
-			int localPortNum = Integer.parseInt(args[0]);
-			DatagramSocket mySocket = new DatagramSocket(localPortNum); byte[] buffer = new byte[MAX_LEN];
-			DatagramPacket packet = new DatagramPacket(buffer, MAX_LEN); mySocket.receive(packet);
-			String message = new String(buffer);
-			System.out.println(message);
-		}
-		catch(Exception e){e.printStackTrace( );}
+        try{
+            int MAX_LEN = 40;
+            int localPortNum = Integer.parseInt(args[0]);
+            DatagramSocket mySocket = new DatagramSocket(localPortNum); byte[] buffer = new byte[MAX_LEN];
+            while(true){
+                DatagramPacket packet = new DatagramPacket(buffer, MAX_LEN); mySocket.receive(packet);
+                String message = new String(buffer);
+                System.out.println(message);
+            }
+        }
+        catch(Exception e){e.printStackTrace( );}
 	}
 }
