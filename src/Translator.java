df5befba27a5486436d77b2646670e29a8524bc2
luis-f-lins <luisfernandolins@gmail.com>
2016-12-09 23:09:24 -0200

    Add files via upload

--- src/Translator.java
+++ src/Translator.java
@@ -1,19 +1,48 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+
 class Translator {
     public static String go(String message) {
-        String to_lang = Translator.parse_to_lang(message);
         message = Translator.clean_message(message);
-        return Translator.message_to(message, to_lang);
+        return Translator.message_to(message);
     }
 
-    static String parse_to_lang(String message) {
-        return null;
-    }
 
     static String clean_message(String message) {
         return message;
     }
 
-    static String message_to(String message, String lang_to) {
-        return message;
+    static String message_to(String message) {
+        char node1 = message.charAt(1);
+        char node2 = message.charAt(2);
+        String command = "trans" + " :" + node1 + node2 + " -brief " + "'" + message.substring(4) + "'";  
+        Process process = null;
+        String trans_msg = null;
+               
+    	    try
+    {
+		process = Runtime.getRuntime().exec(command); 
     }
+    catch(IOException ioe)
+    {
+        ioe.printStackTrace();
+    }                   
+    
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));                                          
+    	    
+    	    try
+    {
+    	trans_msg = reader.readLine(); 
+    }
+    catch(IOException ioe)
+    {
+        ioe.printStackTrace();
+    }     
+    
+        return command;
+    }
+                                                           
+
 }
